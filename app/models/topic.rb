a5ed55389c954e38ecbb70cf342ac772d26f59e3
Julie <julie.lin0812@gmail.com>
2018-04-08 15:55:40 -0700

    Add notes

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,4 @@
 class Topic < ApplicationRecord
   has_many :votes, dependent: :destroy
+# dependent: :destroy on has_many :votes means when a Topic gets destroyed, the associates Votes will be destroyed too.
 end
